4c7b4a33a2b0caa6e1c3a123108a25dda66947ff
Yehia Khaled <yehiakhaled922@gmail.com>
2024-12-23 19:23:55 +0200

    2. Calling Methods: Instructions

--- 11. Methods_C#/Program.cs
+++ 11. Methods_C#/Program.cs
@@ -5,6 +5,40 @@
         static void Main(string[] args)
         {
             //Lesson 11. Methods in C#
+
+            //2. Calling Methods
+            /*Instructions
+            Checkpoint 1 Enabled
+            1.
+            The designer of C# is "Anders Hejlsberg" — we’ve stored his name in a variable called designer. Call Console.WriteLine() using designer as an argument.
+
+            Checkpoint 2 Step instruction is unavailable until previous steps are completed
+            2.
+            Now, let’s print only Anders’ second name.
+
+            First, find the index of the space(" ") in the string designer and store it in a variable indexOfSpace.
+
+            You’ll need to use the IndexOf() method.
+
+            Checkpoint 3 Step instruction is unavailable until previous steps are completed
+            3.
+            The space character itself is not part of the second name.
+
+            Create a new integer indexOfSecondName and instanciate it with a value of indexOfSpace plus 1.
+
+            Checkpoint 4 Step instruction is unavailable until previous steps are completed
+            4.
+            Use Substring() and the variable indexOfSecondName to get the second name.
+
+            Store the returned value in a variable secondName.You can view the documentation if you’re not sure how to use this method.
+
+            Checkpoint 5 Step instruction is unavailable until previous steps are completed
+            5.
+            Print secondName to the console.
+            */
+
+
+
         }
     }
 }
