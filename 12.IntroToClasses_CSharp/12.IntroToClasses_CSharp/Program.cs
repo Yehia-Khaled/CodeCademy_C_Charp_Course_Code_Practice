d9cfcf2478ca76a2d433309a23bb20eb0e4a2de5
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2024-12-28 13:23:07 +0200

    1.Introduction to Classes and Abstraction

--- 12.IntroToClasses_CSharp/12.IntroToClasses_CSharp/Program.cs
+++ 12.IntroToClasses_CSharp/12.IntroToClasses_CSharp/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace _12.IntroToClasses_CSharp
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.IntroToClasses_CSharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //1.Introduction to Classes and Abstraction
+            /*    Instructions
+                Reference the image for some conceptual examples of custom classes.Note that each class is associated with 2 variables and 1 method.The variables represent the state, such as whether or not the lightbulb is on and how many watts it consumes.The methods represent behaviors, such as turning the lightbulb on.
+
+                NOTE: Testing this kind of code takes longer, so some checkpoints in this lesson will need more time to run.It’s worth the wait!
+            */
 
         }
     }
