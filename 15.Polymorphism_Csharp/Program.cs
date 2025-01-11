5e18aaa832fd730b6901fc50ab6195779ad11de7
Yehia-Khaled <yehiakhaled28@gmail.com>
2025-01-11 14:30:02 +0200

    1. Introduction to Polymorphism: Instructions

--- 15.Polymorphism_Csharp/Program.cs
+++ 15.Polymorphism_Csharp/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace _15.Polymorphism_Csharp
 {
@@ -10,7 +12,16 @@ namespace _15.Polymorphism_Csharp
     {
         static void Main(string[] args)
         {
+            //Lesson 15. C#: Polymorphism
+
+            //1. Introduction to Polymorphism
+            /*Instructions
+            The code editor showcases Polymorphism in action.Take a look at the code and 
+            move to the next exercise when you are ready to learn how to implement Polymorphism within your class structure.
+            */
+
 
         }
+
     }
 }
