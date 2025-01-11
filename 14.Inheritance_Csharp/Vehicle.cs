3bcea731de0964b3aad1e7e16663b27bc4e6ad12
Yehia-Khaled <yehiakhaled28@gmail.com>
2025-01-11 10:18:43 +0200

    4. Access Inherited Members with Protected: Code Solution

--- 14.Inheritance_Csharp/Vehicle.cs
+++ 14.Inheritance_Csharp/Vehicle.cs
@@ -53,7 +53,30 @@ namespace _14.Inheritance_Csharp
         Wheels
         */
 
+        //Code Solution
+        public string LicensePlate
+        { get; protected set; }
 
+        public double Speed
+        { get; protected set; }
+
+        public int Wheels
+        { get; protected set; }
+
+        public void SpeedUp()
+        {
+            Speed += 5;
+        }
+
+        public void SlowDown()
+        {
+            Speed -= 5;
+        }
+
+        public void Honk()
+        {
+            Console.WriteLine("HONK!");
+        }
 
     }
 }
