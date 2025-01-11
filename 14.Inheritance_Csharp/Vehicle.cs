e06e3b86e5744bfe2d2579c5327eeafae14c82cc
Yehia-Khaled <yehiakhaled28@gmail.com>
2025-01-11 10:34:15 +0200

    6. Access Inherited Members with Base: Instructions

--- 14.Inheritance_Csharp/Vehicle.cs
+++ 14.Inheritance_Csharp/Vehicle.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using Accord.Collections;
+using Microsoft.VisualStudio.Services.Client.AccountManagement.Logging;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -54,7 +59,7 @@ namespace _14.Inheritance_Csharp
         */
 
         //Code Solution
-        public string LicensePlate
+        /*public string LicensePlate
         { get; protected set; }
 
         public double Speed
@@ -77,6 +82,44 @@ namespace _14.Inheritance_Csharp
         {
             Console.WriteLine("HONK!");
         }
+        */
+
+
+        //6. Access Inherited Members with Base
+        /*Instructions
+         Currently, the Sedan constructor implicitly calls the Vehicle default parameterless constructor, also known as Vehicle().
+
+        Let’s explicitly define a constructor in Vehicle.It should:
+
+        take one parameter, double speed
+        set Speed to speed
+        set LicensePlate to a random value using Tools.GenerateLicensePlate()
+        Note that this is very similar to both the Sedan and Truck constructors.
+
+        After doing this you may see the following error:
+
+        error CS7036: There is no argument given that corresponds to the required formal parameter 'speed' of 'Vehicle.Vehicle(double)'
+
+
+        This is good! It proves to us that the Sedan constructor is calling the parameterless constructor Vehicle(). Now that we have explicitly defined a constructor Vehicle(double speed), there is no more Vehicle(), hence the error.
+
+        Checkpoint 2 Step instruction is unavailable until previous steps are completed
+        2.
+        Back in Sedan.cs:
+
+        Delete the lines in the constructor that set Speed and LicensePlate.
+        Call the superclass constructor using : base(speed).
+        Checkpoint 3 Step instruction is unavailable until previous steps are completed
+        3.
+        Repeat the process in Truck.cs:
+
+        Delete the lines in the constructor that set Speed and LicensePlate.
+        Call the superclass constructor using : base(speed).
+        Checkpoint 4 Step instruction is unavailable until previous steps are completed
+        4.
+        Since the LicensePlate and Speed properties defined in Vehicle are no longer accessed in Sedan or Truck, they no longer need to be protected. Switch those two setters to private.
+        */
+
 
     }
 }
